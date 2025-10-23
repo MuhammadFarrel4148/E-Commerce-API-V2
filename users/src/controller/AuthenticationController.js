89933c052c8839fbacd848f043b95d0ede80c2d3
Muhammad Farrel <farrelputrapambudi.2020@gmail.com>
2025-10-23 20:52:09 +0700

    add authentication controller

---
+++ users/src/controller/AuthenticationController.js
@@ -0,0 +1,30 @@
+class AuthenticationsController {
+    constructor(authenticationsService, usersService, tokenManager, validator) {
+        this._authenticationsService = authenticationsService;
+        this._usersService = usersService;
+        this._tokenManager = tokenManager;
+        this._validator = validator;
+    };
+
+    async addAuthenticationsController(req, res) {
+        await this._validator.validateAuthenticationsPayload(req.body);
+
+        const { username, password } = req.body;
+
+        const userId = await this._usersService.verifyCredential(username, password);
+
+        const accessToken = await this._tokenManager.generateAccessToken({ userId });
+        const refreshToken = await this._tokenManager.generateRefreshToken({ userId });
+        await this._authenticationsService.addTokenService(refreshToken);
+
+        res.status(201).json({
+            status: 'success',
+            data: {
+                accessToken,
+                refreshToken
+            }
+        });
+    };
+};
+
+module.exports = AuthenticationsController;
