444a90ee5cd75fce712d212d3a3774a8212a524e
Muhammad Farrel <farrelputrapambudi.2020@gmail.com>
2025-10-23 20:51:15 +0700

    add verify credential and add token

--- users/src/service/UsersService.js
+++ users/src/service/UsersService.js
@@ -2,6 +2,7 @@ const { Pool } = require('pg')
 const { nanoid } = require('nanoid');
 const bcrypt = require('bcrypt');
 const InvariantError = require('../exceptions/InvariantError');
+const AuthenticationError = require('../exceptions/AuthenticationError');
 
 class UsersService {
     constructor() {
@@ -18,6 +19,28 @@ class UsersService {
         if(result.rows.length > 0) {
             throw new InvariantError('Username telah digunakan');
         };
+    };
+
+    async verifyCredential(username, password) {
+        const query = {
+            text: `SELECT * FROM users WHERE username = $1`,
+            values: [username]
+        };
+        const result = await this._pool.query(query);
+
+        if(!result.rows.length) {
+            throw new AuthenticationError('Kredensial yang diberikan salah');
+        };
+
+        const { user_id: id, password: hashedPassword } = result.rows[0];
+
+        const match = await bcrypt.compare(password, hashedPassword);
+
+        if(!match) {
+            throw new AuthenticationError('Kredensial yang diberikan salah');
+        };
+
+        return id;
     };
 
     async addUsersService(username, password, fullname) {
