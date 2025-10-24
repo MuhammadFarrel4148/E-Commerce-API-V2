1f0d50d80b7c813d0e4202b2a388db2499af467a
Muhammad Farrel <farrelputrapambudi.2020@gmail.com>
2025-10-24 18:59:48 +0700

    add routing to address

--- users/app.js
+++ users/app.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const userRouter = require('./src/routes/UsersRouter');
 const authenticationRouter = require('./src/routes/AuthenticationRouter');
+const addressRouter = require('./src/routes/AddressUsersRouter');
 const ClientError = require('./src/exceptions/ClientError');
 
 const app = express();
@@ -11,6 +12,7 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use('/users', userRouter);
 app.use('/authentications', authenticationRouter);
+app.use('/address', addressRouter);
 app.use((err, req, res, next) => {
     if(err instanceof ClientError) {
         res.status(err.statusCode).json({
---
+++ users/src/routes/AddressUsersRouter.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const AuthorizationsService = require('../middleware/AuthorizationService');
+const AddressUsersController = require('../controller/AddressUsersController');
+const AddressUsersService = require('../service/AddressUsersService');
+const AddressValidator = require('../validator/address/AddressIndex');
+
+const addressRouter = express.Router();
+const authorizationsService = new AuthorizationsService();
+const addressUsersService = new AddressUsersService();
+const addressUsersController = new AddressUsersController(addressUsersService, AddressValidator);
+
+addressRouter.post('/add', authorizationsService.AuthMiddleware, addressUsersController.addAddressUsersController);
+
+module.exports = addressRouter;
