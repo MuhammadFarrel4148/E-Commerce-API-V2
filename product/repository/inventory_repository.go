c66341e30e3c7c063e41abfe10a4424bfea18ec4
Muhammad Farrel <farrelputrapambudi.2020@gmail.com>
2025-10-27 16:16:42 +0700

    add inventory repository

---
+++ product/repository/inventory_repository.go
@@ -0,0 +1,80 @@
+package repository
+
+import (
+	"product/model"
+
+	"gorm.io/gorm"
+)
+
+type InventoryRepository interface {
+	CreateInventory(inventory *model.Inventory) error
+	GetInventoryByID(ID uint) (*model.Inventory, error)
+	UpdateInventoryByID(ID uint, updatesInventory map[string]interface{}) (*model.Inventory, error) 
+	DeleteInventoryByID(ID uint) (*model.Inventory, error)
+}
+
+type inventoryRepository struct {
+	db *gorm.DB
+}
+
+func NewInventoryRepository(db *gorm.DB) InventoryRepository {
+	return &inventoryRepository{db}
+}
+
+func (r *inventoryRepository) CreateInventory(inventory *model.Inventory) error {
+	err := r.db.Create(inventory).Error
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *inventoryRepository) GetInventoryByID(ID uint) (*model.Inventory, error) {
+	var inventory model.Inventory
+
+	err := r.db.First(&inventory, ID).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &inventory, nil
+}
+
+func (r *inventoryRepository) UpdateInventoryByID(ID uint, updatesInventory map[string]interface{}) (*model.Inventory, error) {
+	var inventory model.Inventory
+
+	err := r.db.Model(&inventory).Where("category_id = ?", ID).Updates(updatesInventory).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = r.db.First(&inventory, ID).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &inventory, nil
+}
+
+func (r *inventoryRepository) DeleteInventoryByID(ID uint) (*model.Inventory, error) {
+	var inventory model.Inventory
+
+	err := r.db.First(&inventory, ID).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = r.db.Delete(&inventory).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &inventory, nil
+}
