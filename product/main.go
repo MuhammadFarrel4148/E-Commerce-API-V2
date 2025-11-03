682df8229b481db1964276edb6cadd8e8e21c083
Muhammad Farrel <farrelputrapambudi.2020@gmail.com>
2025-11-03 21:11:58 +0700

    add connect database, routing, and initialize interface in main.go

--- product/main.go
+++ product/main.go
@@ -4,12 +4,30 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"product/controller"
+	"product/database"
+	"product/repository"
+	"product/service"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	database.ConnectDB()
+	db := database.DB
+
+	productRepository := repository.NewProductRepository(db)
+	productService := service.NewProductService(productRepository)
+	productController := controller.NewProductController(productService)
+
 	router := gin.Default()
+
+	api := router.Group("/product/v1")
+
+	api.POST("/product", productController.CreateProduct)
+	api.GET("/product/:id", productController.GetProductByID)
+	api.PUT("/product/:id", productController.UpdateProductByID)
+	api.DELETE("/product/:id", productController.DeleteProductByID)
 
 	port := os.Getenv("SERVER_PORT")
 	if port == "" {
