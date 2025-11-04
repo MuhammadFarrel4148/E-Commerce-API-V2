cae644f90bfa901f23e4038c1ec8bc0b54b478d8
Muhammad Farrel <farrelputrapambudi.2020@gmail.com>
2025-11-04 20:47:52 +0700

    add binding required for input product

--- product/service/product_service.go
+++ product/service/product_service.go
@@ -7,9 +7,9 @@ import (
 )
 
 type CreateProductInput struct {
-	Name       string  `json:"name"`
-	Price      float64 `json:"price"`
-	CategoryID uint    `json:"category_id"`
+	Name       string  `json:"name" binding:"required"`
+	Price      float64 `json:"price" binding:"required`
+	CategoryID uint    `json:"category_id" binding:"required"`
 }
 
 type UpdateProductInput struct {
